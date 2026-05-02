7038466bbe0bfc6ae20e57f648ec56edfeab83aa
Appon Islam <appon2003@gmail.com>
2026-05-02 20:06:33 +0600

    new patter problem solved

---
+++ PhitronOj/Pattern4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    // printf("The value of a is %d", a);
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
